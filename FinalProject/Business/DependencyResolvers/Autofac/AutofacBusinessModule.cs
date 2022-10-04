dc3f6f833ef05b4fae9118166381816d2310dcee
fatihbereketinan <fatihbereketinan@gmail.com>
2022-10-04 15:03:57 +0300

    Yazilim_Gelistirici_Yetistirme_Kampi_13.Gun

---
+++ FinalProject/Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -0,0 +1,35 @@
+﻿using Autofac;
+using Autofac.Extras.DynamicProxy;
+using Business.Abstract;
+using Business.Concrete;
+using Castle.DynamicProxy;
+using Core.Utilities.Interceptors;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.DependencyResolvers.Autofac
+{
+    public class AutofacBusinessModule : Module //using Autofac!!
+    {
+        //Autofac; instance üretimini bizim için yapan yapıdır. //webapi deki startup ın karşılığıdır.
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<ProductManager>().As<IProductService>().SingleInstance();
+            builder.RegisterType<EfProductDal>().As<IProductDal>().SingleInstance();
+            builder.RegisterType<CategoryManager>().As<ICategoryService>().SingleInstance();
+            builder.RegisterType<EfCategoryDal>().As<ICategoryDal>().SingleInstance();
+
+            var assembly = System.Reflection.Assembly.GetExecutingAssembly();  //Aspectleri bul çalıştır.
+
+            builder.RegisterAssemblyTypes(assembly).AsImplementedInterfaces()
+                .EnableInterfaceInterceptors(new ProxyGenerationOptions()
+                {
+                    Selector = new AspectInterceptorSelector()
+                }).SingleInstance();
+
+        }
+    }
+}
